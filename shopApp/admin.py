d991a9d1dfcaa35b29a9dacad1b2463b0eb49a0b
Omar Lopez <omarisaac.lopezgonzalez@gmail.com>
2025-03-18 14:18:03 -0600

    Tarea Contacto

---
+++ shopApp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin 
+from shopApp.models import Product, HistoricalCost, Contacts
+
+# Register your models here.
+admin.site.register(Product)
+admin.site.register(HistoricalCost)
+admin.site.register(Contacts)
+
+
